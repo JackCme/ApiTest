f7dee91a449dc10b0368fa1d8f541641d5a8436e
Won Jun Choi <pitou_106@naver.com>
2016-02-05 16:59:35 +0900

    서블릿 수정

--- src/ApiDataServlet.java
+++ src/ApiDataServlet.java
@@ -90,27 +90,6 @@ public class ApiDataServlet extends HttpServlet {
 		request.setAttribute("gu", gu);
 		request.setAttribute("dong", dong);
 		
-		
-		//Geocode api 
-		/*
-		String clientId = "Fb6yUt0Z8Dy9KXt8oNtc";
-		String clientSecret = "uNk8ZBCBp_";
-		String encoded = URLEncoder.encode("성남시 분당구 야탑동 342-1", "UTF-8");
-		String url = "https://openapi.naver.com/v1/map/geocode?query=" + encoded;
-		 
-		 HttpClient client = HttpClientBuilder.create().build();
-		 HttpGet req = new HttpGet(url);
-		 
-		 req.addHeader("X-Naver-Client-Id", clientId);
-		 req.addHeader("X-Naver-Client-Secret", clientSecret);
-		 HttpResponse res = client.execute(req);
-		 
-		 HttpEntity res_entity = res.getEntity();
-		 String res_string = EntityUtils.toString(res_entity);
-		 
-		 request.setAttribute("res_string", res_string);
-		 
-		*/
 		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/index.jsp");
 		requestDispatcher.forward(request, response);
 	}
